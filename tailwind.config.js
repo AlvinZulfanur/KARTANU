9340ca26cda36a9cff6fed69a745d8b382f6b422
AlvinZulfanur <alvinzulfanur72@gmail.com>
2022-05-16 13:32:20 +0700

    commit on master

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#16a34a',
+        secondary: '#4ade80',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        Inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
